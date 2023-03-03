6cfe6aae9c18e3cec705f9f76364c2c3abe8248b
anniehuang0410 <anniehuang0410@gmail.com>
2023-03-03 17:13:44 +0800

    Add controller and control flow

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ const Symbols = [
   'https://assets-lighthouse.alphacamp.co/uploads/image/file/17988/__.png' // 梅花
 ]
 
+// 定義遊戲狀態
+const GAME_STATE = {
+  firstCardAwaits: 'firstCardAwaits',
+  secondCardAwaits: 'secondCardAwaits',
+  cardMatchFailed: 'cardMatchFailed',
+  cardMatched: 'cardMatched',
+  gameFinished: 'gameFinished'
+}
+
 // 不適合歸類在 MVC 中
 const utility = {
   // 洗牌：Fisher-Yates shuffle
@@ -52,27 +61,90 @@ const view = {
     }
   },
   // 展示卡片
-  displayCards() {
+  displayCards(indexes) {
     const rootElement = document.querySelector('#cards')
-    rootElement.innerHTML = utility.getRandomNumberArray(52).map(index => this.getCardElement(index)).join('')
+    rootElement.innerHTML = indexes.map(index => this.getCardElement(index)).join('')
   },
   // 翻卡動作
   flipCard(card) {
     if(card.classList.contains('back')) {
       // 回傳正面
       card.classList.remove('back')
-      card.innerHTML = this.getCardContent(card.dataset.index)
+      card.innerHTML = this.getCardContent(Number(card.dataset.index))
       return
     }
     // 回傳背面
     card.classList.add('back')
     card.innerHTML = null
+  },
+  // 配對成功者樣式變化
+  pairedCards(card) {
+    card.classList.add('paired')
   }
 }
-view.displayCards()
+
+const model = {
+  // 管理翻開的牌
+  revealedCards: [],
+  // 檢查翻開的牌是否配對
+  isRevealedCardsMatched() {
+    return this.revealedCards[0].dataset.index % 13 === this.revealedCards[1].dataset.index % 13
+  }
+}
+
+const controller = {
+  // 現在遊戲狀態
+  currentState: GAME_STATE.firstCardAwaits,
+  // 由 controller 中產生牌
+  generateCards() {
+    view.displayCards(utility.getRandomNumberArray(52))
+  },
+  // 狀態控制器
+  dispatchCardAction(card) {
+    // 如果卡片停在正面，不再翻牌
+    if (!card.classList.contains('back')) {
+      return 
+    }
+    // 狀態切換
+    switch (this.currentState) {
+      // 狀態一：：等待翻第一張牌
+      case GAME_STATE.firstCardAwaits:
+        view.flipCard(card)
+        model.revealedCards.push(card)
+        this.currentState = GAME_STATE.secondCardAwaits
+        break // 這個動作暫停，但下面的函式可以繼續運行
+      // 狀態二：：等待翻第二張牌
+      case GAME_STATE.secondCardAwaits:
+        view.flipCard(card)
+        model.revealedCards.push(card)
+        // 判斷是否配對成功
+        if (model.isRevealedCardsMatched()) {
+          // 狀態三：配對成功
+          this.currentState = GAME_STATE.cardMatched
+          view.pairedCards(model.revealedCards[0])
+          view.pairedCards(model.revealedCards[1])
+          model.revealedCards = []
+          this.currentState = GAME_STATE.firstCardAwaits
+        } else {
+          // 狀態四：配對失敗
+          this.currentState = GAME_STATE.cardMatchFailed
+          setTimeout(() => {
+            view.flipCard(model.revealedCards[0])
+            view.flipCard(model.revealedCards[1])
+            model.revealedCards = []
+            this.currentState = GAME_STATE.firstCardAwaits
+          }, 1000)
+        }
+        break 
+    }
+    console.log('this.currentState', this.currentState)
+    console.log('revealedCards', model.revealedCards.map(card => card.dataset.index))
+  }
+}
+controller.generateCards()
 
 document.querySelectorAll('.card').forEach(card => {
   card.addEventListener('click', event => {
-    view.flipCard(card)
+    controller.dispatchCardAction(card)
   })
 })
